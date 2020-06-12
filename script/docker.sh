d16e5187b568f22df70f0e65628024c40fc6080e
misbahmehmood <misbaharmehmood@gmail.com>
2020-06-12 13:46:40 +0000

    changing for update

--- script/docker.sh
+++ script/docker.sh
@@ -2,10 +2,4 @@
 
 source ~/.bashrc
 
-docker-compose build
-docker push misbahmehmood/service_1
-docker push misbahmehmood/service_2
-docker push misbahmehmood/service_3
-docker push misbahmehmood/service_4
-
 env PROJECT_URI="${PROJECT_URI}" docker stack deploy -c docker-compose.yaml fullapp
